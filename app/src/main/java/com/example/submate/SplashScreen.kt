46402edef3f58575361255e04b24ee2d492b5ef0
Robin O. Almorfi Jr <111577379+robinalmorfi@users.noreply.github.com>
2023-08-16 20:56:26 +0800

    initial splash screen

--- app/src/main/java/com/example/submate/SplashScreen.kt
+++ app/src/main/java/com/example/submate/SplashScreen.kt
@@ -3,18 +3,19 @@ package com.example.submate
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.ImageView
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        iv_note.alpha = 0f
-        iv_note.animate().setDuration(1500).alpha(1f).withEndAction {
-        val i = Intent(this, MainActivity::class.java)
+        val ivNote: ImageView = findViewById(R.id.iv_note)
+        ivNote.alpha = 0f
+        ivNote.animate().setDuration(1500).alpha(1f).withEndAction {
+            val i = Intent(this@SplashScreen, MainActivity::class.java)
             startActivity(i)
-            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
         }
     }
