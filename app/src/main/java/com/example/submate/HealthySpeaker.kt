528885b3e1c322efad48b046376363d1df79502c
Robin O. Almorfi Jr <111577379+robinalmorfi@users.noreply.github.com>
2023-09-13 00:50:22 +0800

    main

---
+++ app/src/main/java/com/example/submate/HealthySpeaker.kt
@@ -0,0 +1,11 @@
+package com.example.submate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HealthySpeaker : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_healthy_speaker)
+    }
+}
--- app/src/main/java/com/example/submate/MainMenu.kt
+++ app/src/main/java/com/example/submate/MainMenu.kt
@@ -1,11 +1,18 @@
 package com.example.submate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        val deafButton = findViewById<Button>(R.id.deaf_button)
+        deafButton.setOnClickListener {
+            val Intent = Intent(this, DeafUser::class.java )
+        }
     }
 }
