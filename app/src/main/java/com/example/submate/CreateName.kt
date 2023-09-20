413c949d691c38aa73adb0134ce56860eba989c6
Robin O. Almorfi Jr <111577379+robinalmorfi@users.noreply.github.com>
2023-09-21 02:43:12 +0800

    main

--- app/src/main/java/com/example/submate/CreateName.kt
+++ app/src/main/java/com/example/submate/CreateName.kt
@@ -1,11 +1,20 @@
 package com.example.submate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class CreateName : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_name)
+
+        val confirmButton = findViewById<Button>(R.id.name_button)
+        confirmButton.setOnClickListener {
+            val intent = Intent(this, MainMenu::class.java)
+            startActivity(intent)
+        }
     }
 }
