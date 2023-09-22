f9382d7073d4eef9772cd41d670d511c4e49fc27
Robin O. Almorfi Jr <111577379+robinalmorfi@users.noreply.github.com>
2023-09-22 10:21:56 +0800

    main

---
+++ app/src/main/java/com/example/submate/Transcription.kt
@@ -0,0 +1,11 @@
+package com.example.submate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Transcription : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transcription)
+    }
+}
