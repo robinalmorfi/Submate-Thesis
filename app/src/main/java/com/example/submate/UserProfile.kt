aec3cbfa4055da98fd2410ec68992f37e2263a00
Robin O. Almorfi Jr <111577379+robinalmorfi@users.noreply.github.com>
2023-09-28 21:29:44 +0800

    main

--- app/src/main/java/com/example/submate/UserProfile.kt
+++ app/src/main/java/com/example/submate/UserProfile.kt
@@ -1,7 +1,6 @@
 package com.example.submate
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class UserProfile : AppCompatActivity() {
@@ -9,12 +8,5 @@ class UserProfile : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
 
-        // Retrieve the entered text from the Intent
-        val intent = intent
-        val enteredText = intent.getStringExtra("name")
-
-        // Display the text in a TextView or any other view as needed
-        val textView = findViewById<TextView>(R.id.app_name_textview)
-        textView.text = enteredText
     }
 }
